e81a1ac2ca57df424d6f170c78fb046715e15b4b
Justin Alfonso <justinalfonso29@hotmail.com>
2020-09-12 12:03:20 +0200

    petition22

--- public/script.js
+++ public/script.js
@@ -3,37 +3,17 @@
     const backDrop = canvas.getContext("2d");
     const ctx = canvas.getContext("2d");
     let hiddenInput = document.getElementById("inputHidden");
-    const clear = document.querySelector(".clearButton");
+    // let left = canvas.offsetLeft;
+    // let top = canvas.offsetTop;
 
     // console.log("hiddeninput Value: ", hiddenInputValue);
     console.log("hiddeninput Value new: ", hiddenInput.value);
     // console.log("line 7 script: ", dataURL);
+    backDrop.beginPath();
+    backDrop.fillStyle = "rgb(230, 227, 227)";
+    backDrop.fillRect(0, 0, 400, 70);
 
     let signature = false;
-
-    canvas.addEventListener("mousedown", () => {
-        signature = true;
-    });
-    canvas.addEventListener("touchstart", () => {
-        console.log("weeeeeee");
-        signature = true;
-    });
-
-    canvas.addEventListener("mouseup", () => {
-        signature = false;
-        const dataURL = canvas.toDataURL();
-        console.log("dataUrl: ", dataURL);
-        hiddenInput.value = dataURL;
-        ctx.beginPath();
-    });
-
-    canvas.addEventListener("touchend", () => {
-        signature = false;
-        const dataURL = canvas.toDataURL();
-        console.log("dataUrl: ", dataURL);
-        hiddenInput.value = dataURL;
-        ctx.beginPath();
-    });
 
     canvas.addEventListener("mouseenter", () => {
         ctx.beginPath();
@@ -43,9 +23,18 @@
         signature = false;
     });
 
-    backDrop.beginPath();
-    backDrop.fillStyle = "rgb(230, 227, 227)";
-    backDrop.fillRect(0, 0, 400, 70);
+    canvas.addEventListener("mousedown", (e) => {
+        signature = true;
+        ctx.beginPath();
+        ctx.moveTo(e.offsetX, e.offsetY);
+    });
+
+    canvas.addEventListener("mouseup", () => {
+        signature = false;
+        const dataURL = canvas.toDataURL();
+        console.log("dataUrl: ", dataURL);
+        hiddenInput.value = dataURL;
+    });
 
     canvas.addEventListener("mousemove", (e) => {
         if (!signature) {
@@ -57,16 +46,41 @@
         ctx.stroke();
     });
 
+    canvas.addEventListener("touchstart", (e) => {
+        e.preventDefault();
+        signature = true;
+        let touchX = e.touches[0].clientX;
+        let touchY = e.touches[0].clientY;
+        ctx.beginPath();
+        ctx.moveTo(
+            touchX - canvas.getBoundingClientRect().left,
+            touchY - canvas.getBoundingClientRect().top
+        );
+        console.log(touchX, touchY);
+    });
+
+    canvas.addEventListener("touchend", (e) => {
+        e.preventDefault();
+        signature = false;
+        const dataURL = canvas.toDataURL();
+        console.log("dataUrl: ", dataURL);
+        hiddenInput.value = dataURL;
+    });
+
     canvas.addEventListener("touchmove", (e) => {
         if (!signature) {
             return;
         }
-        console.log("weeeeeee");
+
+        let touchX = e.touches[0].clientX;
+        let touchY = e.touches[0].clientY;
+        console.log(touchX, touchY);
         ctx.lineWidth = 2;
         ctx.lineCap = "round";
-        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.lineTo(
+            touchX - canvas.getBoundingClientRect().left,
+            touchY - canvas.getBoundingClientRect().top
+        );
         ctx.stroke();
     });
-
-    // canvas.addEventListener("mousemove", (e) => {});
 })(); //iffe close
